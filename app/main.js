37b189affd44e6a81061fe164c1165c46d5d4018
greedybrute <asharma1_be20@thapar.edu>
2024-07-29 14:39:39 +0530

    pass stage

--- app/main.js
+++ app/main.js
@@ -336,10 +336,11 @@ async function  main() {
          }
       }).then((res)=>{
         const result=res.data;
+        console.log(result)
         // const data=Buffer.from(result,'binary');
-        const decodedValue=decodeBencode(res.data);
+        const decodedValue=decodeBencode(result.toString('binary'));
         // console.log(decodedValue)
-        const peersBuffer= Buffer.from(decodedValue.peers,'binary')
+        const peersBuffer= Buffer.from(decodedValue.peers.toString('binary'),'binary')
         // console.log(hexToByte(bufferedValue.toString('hex')))
         // for(let i of bufferedValue)
         // {
